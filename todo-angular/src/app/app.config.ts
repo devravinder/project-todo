d612a077a222a42279e40047d275fb3f2e241e8a
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-07-23 16:55:14 +0530

    feat: PWA support

--- todo-angular/src/app/app.config.ts
+++ todo-angular/src/app/app.config.ts
@@ -1,18 +1,23 @@
 import {
   ApplicationConfig,
+  isDevMode,
   provideAppInitializer,
-  provideBrowserGlobalErrorListeners
+  provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import onAppStart from './util/initializer';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAppInitializer(onAppStart),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withHashLocation()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
